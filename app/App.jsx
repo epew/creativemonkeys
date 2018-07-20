27a8d6cf739c07f69ab42321be0c53c980e9d834
epew <madalina.tivlea@wirtek.com>
2018-07-20 15:06:16 +0300

    First commit

---
+++ app/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>creative monkeys, hello world!!!</div>
+    );
+  }
+}
+
+export default App;
